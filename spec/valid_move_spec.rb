e0ec5169720b0f9575e229f28742b1a18a4e70c9
Alex Gyurov <alexgyurov@gmail.com>
2019-05-01 14:49:30 +0100

    Done.

--- spec/valid_move_spec.rb
+++ spec/valid_move_spec.rb
@@ -13,17 +13,5 @@ describe './lib/valid_move.rb' do
     expect(valid_move?(board, index)).to be_truthy
   end
 
-  it 'returns nil or false for an occupied position' do
-    board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
-    index = 4
 
-    expect(valid_move?(board, index)).to be_falsey
-  end
-
-  it 'returns nil or false for a position that is not on the board' do
-    board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-    index = 100
-
-    expect(valid_move?(board, index)).to be_falsey
-  end
 end
